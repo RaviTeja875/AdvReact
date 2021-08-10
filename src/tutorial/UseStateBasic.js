891475d7d0104bccc573ebbaba18a6f3553af327
Ravi Teja <raviteja96763@gmail.com>
2021-08-10 18:05:26 +0530

    click responses

---
+++ src/tutorial/UseStateBasic.js
@@ -0,0 +1,13 @@
+import React , {useState} from 'react'
+//use state is a named import so we must  must have the curly braces
+function UseStateBasic() {
+    console.log(useState("Neeraj Chopra"));
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default UseStateBasic
--- src/tutorial/useStateErrorEx.js
+++ src/tutorial/useStateErrorEx.js
@@ -3,16 +3,18 @@ import React from 'react'
 function UseStateerrorEx() {
     let title ="random title";//its initial value is random title
     const handleClick= () => {
-        console.log(title)
-        //title = "My name is title";
+       
+        title = "My name is title";
+        console.log(title);
+
     }
     return (<React.Fragment>
         <h2>{title}</h2>
-        <button type="button" className="btn" onClick={handleClick} >
+        <button type="button" className="btn" onClick={handleClick(800000)} >
             change title
         </button>
         </React.Fragment>
-    )
-}
+    );
+};
 
 export default UseStateerrorEx
