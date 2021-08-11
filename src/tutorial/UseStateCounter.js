b3bd76296faf3b69995b3c608bc46108f4ddefc5
Ravi Teja <raviteja96763@gmail.com>
2021-08-11 11:53:45 +0530

    setting a timer  for increasing the value

--- src/tutorial/UseStateCounter.js
+++ src/tutorial/UseStateCounter.js
@@ -2,6 +2,11 @@ import React, {useState} from 'react'
 
 function UseStateCounter() {
     const [value,setValue] = useState(0);
+    const complexIncrease = () => {
+        setTimeout(()=>{
+            setValue(value +1);
+        }, 4000)
+    }
     return (
         <>
         <section style={{margin: '4rem 0'}}>
@@ -12,6 +17,12 @@ function UseStateCounter() {
             <button className='btn' onClick={() => setValue(0)}> reset </button>
 
 
+
+        </section>
+        <section style={{margin: '4rem 0'}}>
+        <h2>a more complex counter</h2>
+        <h1>{value}</h1>
+        <button className="btn" onClick={complexIncrease}> Increase Later</button>
 
         </section>
       </>
