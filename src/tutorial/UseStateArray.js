a6cc61e53a94939a959e1f96fe928e9b4c30427f
Ravi Teja <raviteja96763@gmail.com>
2021-08-11 11:01:09 +0530

    removing the data

--- src/tutorial/UseStateArray.js
+++ src/tutorial/UseStateArray.js
@@ -3,14 +3,20 @@ import { data } from "./data";
 
 function UseStateArray() {
     const [people,setPeople] = React.useState(data)
+    const removeItem = (id) => {
+        let newPeople = people.filter((person) => person.id !==id );
+        setPeople(newPeople);
+    }
     return (
         <>
         {
-            people.map((person) => {
+            people.map((person) => 
+            {
                 const {id,name} = person;
                 return(
                     <div key={id} className ='item'>
                         <h4>{name}</h4>
+                        <button onClick={() => removeItem(id)}> remove him </button>
                     </div>
                 )
             }
