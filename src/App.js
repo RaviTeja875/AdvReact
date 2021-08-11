0b9aa6b6861c8061a70d8d02258a167c74e18dfd
Ravi Teja <raviteja96763@gmail.com>
2021-08-11 13:02:06 +0530

    using use effect to display name on apage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
-//import UseStateErrorex from "./tutorial/useStateErrorEx";
-import UseStateBasic from './tutorial/UseStateBasic';
-import UseStateArray from './tutorial/UseStateArray';
-import UseStateObject from './tutorial/UseStateObject';
-//import UseStateObject from './tutorial/UseStateObject';
-import UseStateCounter from  './tutorial/UseStateCounter';
+import UseEffectBasics from './tutorial/UseEffectBasics';
 
 
 function App() {
   return ( <div className='container'>
     <h2>Advance React js</h2>
    
-    <UseStateCounter />
+    <UseEffectBasics />
   </div>
   );
 }
