ff4da8d09d7dbb83d8dc247e07617ad564ef5735
Ravi Teja <raviteja96763@gmail.com>
2021-08-11 10:47:57 +0530

    change message in button

--- src/tutorial/UseStateBasic.js
+++ src/tutorial/UseStateBasic.js
@@ -1,6 +1,11 @@
 import React , {useState} from 'react'
 //use state is a named import so we must  must have the curly braces
-function UseStateBasic() {
+// some basic rules of all hooks basically any import/function that starts with use 
+//components name must be in upper case
+//must be in the function component body
+//cannot call conditionally
+function UseStateBasic() 
+{
     const [day,setDay]= useState("Today is  a rainy day");
     const handleClick =() => {
         if(day ==="Today is  a rainy day:("){
---
+++ src/tutorial/data.js
@@ -0,0 +1,7 @@
+export const data =[
+    {id:1,name:"Tony Stark"},
+    {id:2,name:"Thor"},
+    {id:3,name:"Groot"},
+    {id:4,name:"Captain America"},
+    
+];
