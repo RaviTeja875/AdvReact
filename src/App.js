4420535a5a93e1e94dbebd6dec0ead4b299194b5
Ravi Teja <raviteja96763@gmail.com>
2021-08-11 11:41:35 +0530

    Increasing and deacreasing the values

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import UseStateBasic from './tutorial/UseStateBasic';
 import UseStateArray from './tutorial/UseStateArray';
 import UseStateObject from './tutorial/UseStateObject';
 //import UseStateObject from './tutorial/UseStateObject';
+import UseStateCounter from  './tutorial/UseStateCounter';
 
 
 function App() {
   return ( <div className='container'>
     <h2>Advance React js</h2>
    
-    <UseStateObject />
+    <UseStateCounter />
   </div>
   );
 }
