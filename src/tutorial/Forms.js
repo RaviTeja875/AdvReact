fd827ea2802214d5489bfd944b5a07565abf93fc
Ravi Teja <raviteja96763@gmail.com>
2021-08-11 15:49:00 +0530

    setting firstname and email

---
+++ src/tutorial/Forms.js
@@ -0,0 +1,39 @@
+import React, {useState} from 'react'
+//we want to have two input fields
+//we want to have abutton
+//one of the input field should accept username
+//email
+//we want to accept the data and store it somewhere in avariable
+//initial state will be empty
+
+function Forms() {
+    const [firstName,setFirstName] = useState("");
+    const [email, setEmail] = useState("");
+
+    const handleSubmit =(e) =>{
+        e.preventDefault();//this is used to prevent default behaiviour of our browser
+        console.log("firstName,email")
+    }
+
+    
+    return (
+        <>
+        <article>
+            <form className='form' onSubmit={handleSubmit}>
+            <div className='form-control'>
+                <lable htmlFor="firstName">Name:</lable>
+                <input type="text" id="firstname" name='firstname' value={firstName} onChange={(e) => setFirstName(e.target.value)}></input>
+
+            </div>
+            <div className='form-control'>
+                <lable htmlFor="email">Name:</lable>
+                <input type="text" id="email" name='email' value={email} onChange={(e) => setEmail(e.target.value)}></input>
+             </div>
+
+            <button type="submit">Submit</button>
+            </form>
+        </article>
+        </>
+    )
+}
+export default  Forms
