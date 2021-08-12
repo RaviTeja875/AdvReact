eda8d42359bf38076b858e86405fbbb18565de9e
Ravi Teja <raviteja96763@gmail.com>
2021-08-12 11:26:32 +0530

    Getting the data from json server

--- src/tutorial/LearnJSON.js
+++ src/tutorial/LearnJSON.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 function LearnJSON() {
     const handleClick = (b) => {
         const promise = axios.post("http://localhost:4000/posts",{//setting the port code
-            name: "ravi" },{ // setting up the object we want or create
+            name: "Harsha" },{ // setting up the object we want or create
             headers: {
                 ['content-type']: 'application/json' //setting the server type
             }}
@@ -18,12 +18,20 @@ function LearnJSON() {
             let c = b ;
             console.log(c);
         }
+        const getUsers = () =>
+        {
+            axios.get("http://localhost:4000/posts").then(users => {
+                {/*we are getting he data from our json server and then starting it in users and then consolingthe
+                array of the objects*/}
+                console.log(users);
+            })
+        }
     return (
         <>
-        <h2>Learning json ,axios</h2>
+        <h2>Learning json</h2>
         <button type ="button" className='btn' onClick={handleClick}> post data </button>
-        {/*<button type ="button" className='btn' onClick={getUsers}> get data </button>
-        */}</>
+        <button type ="button" className='btn' onClick={getUsers}> get data </button>
+        </>
 
       
     )
