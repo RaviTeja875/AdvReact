58b69b2b4142403d307ff0a8ea3bb9b49e091a9a
Ravi Teja <raviteja96763@gmail.com>
2021-08-13 09:37:15 +0530

    learning about ReactRouterDom

---
+++ src/tutorial/React-Router/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import About from './About';
+import Home from './Home';
+import Error from './Error';
+import NavBar from './NavBar';
+import People from './People';
+import Person from './Person';
+//import {Bro}
+
+function ReactRouterDom() {
+    return (
+        <div>
+        <h2>We are Learning About React Router Dom</h2>
+    </div>
+    )
+}
+
+export default ReactRouterDom
